50e64931b5f51972320d5b94f2964a133077f111
AnastasiaShipitsyna <mazya94@mail.ru>
2017-06-21 18:21:44 +0500

    Lab 3

---
+++ lab3/SummatorHost/SummatorHost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using SummatorRemoteService;
+
+namespace SummatorHost
+{
+    class SummatorHost
+    {
+        static void Main(string[] args)
+        {
+            var channal = new TcpChannel(51495);
+            ChannelServices.RegisterChannel(channal, false);
+            var service = new SummatorRemoteService.SummatorRemoveService();
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(SummatorRemoteService.SummatorRemoveService), 
+                "Summator", WellKnownObjectMode.SingleCall);
+
+            Console.WriteLine("Servise work");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ lab3/SummatorRemoteService/Summator RemoveService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SummatorRemoteService
+{
+    public class SummatorRemoveService : MarshalByRefObject, ISummatorRemoveService
+    {
+        public int GetSum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
